4f75e37d106901e941027587d82911dc69bacf27
Rustam Mehmandarov <mehmandarov@gmail.com>
2021-05-23 15:41:55 +0200

    Added atrifact from start.microprofile.io

---
+++ src/main/java/com/mehmandarov/randomstrings/RandomstringsRestApplication.java
@@ -0,0 +1,11 @@
+package com.mehmandarov.randomstrings;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class RandomstringsRestApplication extends Application {
+}
